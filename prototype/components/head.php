442d2942d1266b6edb9314000c56c8ce0b0e37ad
Kerollos Gamal <36204748+kerollosgamal97@users.noreply.github.com>
2018-12-02 18:36:44 +0200

    Add files via upload

---
+++ prototype/components/head.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="./images/favicon.ico">
+
+    <title>EG Fashion</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/EGFashion.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top " style="background:black;">
+        <a class="navbar-brand" href="#">EG Fashion</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="signin.php">Sign In</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
---
+++ prototype/components/tail.php
@@ -0,0 +1,16 @@
+    </main>
+
+    <footer class="footer">
+      <div class="container"style='text-align: center;'>
+        <span class="text-muted" >Copyright 2018, EG Fashion.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery.min.js">
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
+</html>
